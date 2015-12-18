f4bb3dd7290b9a891383eb8d1adceefdfe3c274e
Loafter <mopper@yandex.ru>
2015-12-18 23:14:30 +0300

    first release

--- app/main.go
+++ app/main.go
@@ -10,7 +10,7 @@ func main() {
 	server := flag.String("server", "127.0.0.1", "a string")
 	aet := flag.String("aet","AE_ARCH1", "a string")
 	target := flag.String("target", "c:\\Temp", "a string")
-	parallel:=flag.Int("parallels", 14, "an int")
+	parallel:=flag.Int("parallels",12, "an int")
 	flag.Parse()
 	if err:= dicommigration.WalkAndSend(*target,*server,string(*port),*aet ,*parallel);err!=nil{
 		log.Println(err)
--- dicommigration/dicomwalker.go
+++ dicommigration/dicomwalker.go
@@ -5,26 +5,27 @@ import (
 	"path/filepath"
 	"log"
 )
-
-func WalkAndSend(dir string,srv string,port string,aet string ,par int)error{
+func WalkAndSend(dir string, srv string, port string, aet string, par int) error {
 	if _, err := os.Stat(dir); err != nil {
 		return err
 	}
-
-	pb:=parralels.ParralelsBallancer{MaxParralels:par,Pb:ParallelRawDicomSend{}}
-	filepath.Walk(dir,func(path string, info os.FileInfo, err error)error{
-		if pb.ActiveJobs()<(par*4){
-			if !info.IsDir() {
-				log.Println("info: add job ----", pb.ActiveJobs())
-				dd := DicomSendData{Server:srv, Port:port, AET:aet, FileName:path}
-				pb.StartNew(dd)
+	i:=0
+	pb := parralels.ParralelsBallancer{MaxParralels:par, Pb:ParallelRawDicomSend{}}
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		i++
+		log.Println(info.Name())
+		if !info.IsDir() {
+			if pb.SleepedJobs() >= par * 8 {
+				pb.WaitAll()
+				log.Println("info: wait done")
 			}
-		}else {
-			log.Println("info: wait done")
-			pb.WaitAll()
-
+			dd := DicomSendData{Server:srv, Port:port, AET:aet, FileName:path}
+			pb.StartNew(dd)
 		}
+
 		return nil
 	})
-return nil
+	pb.WaitAll()
+	log.Println(i)
+	return nil
 }
